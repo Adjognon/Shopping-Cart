e176c68b3e370251f252fa69f859a28550e3b37c
Michel ADJOGNON <kadjogno@isi-mtl.com>
2020-11-10 22:07:04 -0500

    création de la fonction addProductToCart pour ajouter un produit au panier - L'entrée des données (Protuits) provient d'un fichier JSON nommé, products.json

--- php/ShoppingCart.php
+++ php/ShoppingCart.php
@@ -5,6 +5,16 @@ session_start();
 class ShoppingCart {
     
 
+    static public function addProductToCart($product){
+
+        if(!isset($_SESSION['cart'])){
+        $_SESSION['cart'] = array();
+        }
+
+        array_push($_SESSION['cart'], $product);
+        
+        return true;
+    }
 
 
 
