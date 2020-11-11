cc376ad36c459b46648d068d48fb473c7cdff2c9
Michel ADJOGNON <kadjogno@isi-mtl.com>
2020-11-11 00:42:40 -0500

    debugage

--- php/ShoppingCart.php
+++ php/ShoppingCart.php
@@ -7,9 +7,8 @@ class ShoppingCart {
     static public function addProductToCart($product){
 
         if(!isset($_SESSION['cart'])){
-           $_SESSION['cart'] = array();
-        }  
-
+        $_SESSION['cart'] = array();
+        }
         array_push($_SESSION['cart'], $product);
         return true;
     }
