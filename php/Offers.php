cbf7cc9247bb0ab5890e3d05f516713331259eb0
Michel ADJOGNON <kadjogno@isi-mtl.com>
2020-11-10 22:48:30 -0500

    On parcourt la liste des produits présents dans le panier ($_SESSION['cart']), et on recupere le nom des produits, si le nom du produit trouvé est Pomme, alors on increment $nb_pomme a 1 ($nb_pomme++) et si le $nb_pomme egale a l'offree de pomme(POMME_OFFER = 2), ne rajoute pas prix, rajoute plutot, 0, et initialise le nombre de pomme $nb_pomme a 0, sinon, additionne le prix

--- php/Offers.php
+++ php/Offers.php
@@ -8,7 +8,29 @@ class Offers extends ShoppingCart{
     const ORANGE_OFFER = 3;
 
     public function paymentWithOffers(){
+        $finalPrice = 0;
+        $nb_pomme = 0;
+        $nb_orange = 0;
+        $first_pomme_index_found = 0;
+        $first_orange_index_found = 0;
+        if(isset($_SESSION['cart'])){
+        $products_keys = array_keys($_SESSION['cart']);
+        foreach($products_keys as $product_key){
+            if($_SESSION['cart'][$product_key]['name'] === 'Pomme'){
+                $nb_pomme++;
+                if($nb_pomme === self::POMME_OFFER){
+                    $finalPrice += 0;
+                    $nb_pomme = 0;
+                    $_SESSION['cart'][$product_key]['price'] = 0;
+                }else{
+                    $finalPrice += $_SESSION['cart'][$product_key]['price'];
+                }
+            }else{
+                $finalPrice += $_SESSION['cart'][$product_key]['price'];
+            }
+        }
 
+    }
 
     }
 }
