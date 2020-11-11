d2ace8dac0571de090e7b4415a0757e14ca4aa0e
Michel ADJOGNON <kadjogno@isi-mtl.com>
2020-11-10 22:12:53 -0500

    Création de la méthode payment qui boucle sur la liste des produits enrégistrée dans la session $_SESSION['cart'], recupere le prix de chaque produit a une position specifique, et les additionnes, stocke le prix total ($finalPrice) a payé dans une session $_SESSION['final_price']

--- php/ShoppingCart.php
+++ php/ShoppingCart.php
@@ -16,6 +16,23 @@ class ShoppingCart {
         return true;
     }
 
+    public function payment(){
+        $finalPrice = 0;
+        if(isset($_SESSION['cart'])){
+        $products_name = array_keys($_SESSION['cart']);
+        
+        foreach($products_name as $product_name){
+           $finalPrice += $_SESSION['cart'][$product_name]['price'];
+           
+        }
+    }
+
+    $_SESSION['nb_products'] = (isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0);
+    $_SESSION['final_price'] = $finalPrice;
+        
+    }
+
+
 
 
 }
